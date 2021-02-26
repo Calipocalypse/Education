7b5cd126abf3e5cb65ec2c5f6c6c47e57628a9ed
Maciej Ziółkowski <maciej.ziolkowski@microsoft.wsei.edu.pl>
2021-02-26 14:06:25 +0100

    Dodaj pliki projektów.

---
+++ Education/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Education
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Education GitHub Project");
+        }
+    }
+}
