56abadb68e69f622ca0ebf66b39f4d95f7c07da5
Maciej Ziółkowski <maciej.ziolkowski@microsoft.wsei.edu.pl>
2021-02-26 14:15:19 +0100

    Created base structure of project

---
+++ Education/1 Yield/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Education._1_Yield
+{
+    public static class Program
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Yield here");
+        }
+
+    }
+}
--- Education/Program.cs
+++ Education/Program.cs
@@ -7,6 +7,13 @@ namespace Education
         static void Main(string[] args)
         {
             Console.WriteLine("Education GitHub Project");
+
+            switch (1)
+            {
+                case 1: _1_Yield.Program.Run();
+                    break;
+                default: Console.WriteLine("Choosen wrong one");
+            }
         }
     }
 }
